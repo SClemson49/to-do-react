967bb2b1e67a49d0563de162fa5efccb6347a36d
SClemson49 <s.clemson49@gmail.com>
2023-01-12 08:30:54 -0900

    finished deliverable

--- src/MyList.jsx
+++ src/MyList.jsx
@@ -5,7 +5,24 @@ import ListItem from './ListItem';
 
 class MyList extends Component {
   state={
-    taskArray: this.props.theList
+    taskArray: this.props.theList,
+    newItem: '',
+  }
+  handleChange = e => {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+  addItem = e => {
+    e.preventDefault()
+    this.setState(prevState => {
+      return{
+        taskArray: [
+          ...prevState.taskArray, prevState.newItem
+        ],
+        newItem:''
+      }
+    })
   }
   clearList = () => {
     this.setState( {
@@ -19,6 +36,16 @@ class MyList extends Component {
     return (
       <div>
       <h1> Things I should stop procrastinating:</h1>
+      <form onSubmit={this.addItem}>
+        <label htmlFor='newItem'>New Item:</label>
+        <input 
+        type='type'
+        placeholder='New Item...'
+        onChange={this.handleChange}
+        value={this.state.newItem}
+        />
+        <button type='submit'>Submit</button>
+      </form>
       <ul>
         {todoItems}
       </ul>
